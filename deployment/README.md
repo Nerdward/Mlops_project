4a9b16f4d55df65499f921c89c03dfa6a39ba304
Ohakim Nnaemeka <ohakim.nnaemeka@gmail.com>
2022-09-05 14:22:55 +0000

    added tests and integration tests

--- deployment/README.md
+++ deployment/README.md
@@ -1,1 +1,31 @@
 mlflow server --backend-store-uri sqlite:///backend.db --default-artifact-root ./artifacts
+
+```bash
+aws s3 cp --recursive model/ s3://nerdward-bucket/model/
+```
+
+```bash
+docker build -t house-price-prediction:v1 .
+
+docker run -it --rm \
+    -p 8080:8080 \
+    -v /home/azureuser/.aws:/root/.aws \
+    house-price-prediction:v1
+```
+
+aws ecr create-repository --repository-name duration-model
+
+aws ecr get-login-password | docker login --username AWS --password-stdin 435520236541.dkr.ecr.us-east-1.amazonaws.com/house-prediction
+
+
+REMOTE_URI="public.ecr.aws/r2u6t4r3/house-prediction"
+REMOTE_TAG="v1"
+REMOTE_IMAGE=${REMOTE_URI}:${REMOTE_TAG}
+
+LOCAL_IMAGE="house-price-prediction:v1"
+docker tag ${LOCAL_IMAGE} ${REMOTE_IMAGE}
+docker push ${REMOTE_IMAGE}
+
+aws ecr-public create-repository \
+     --repository-name house-prediction \
+     --region us-east-1
---
+++ deployment/lambda_function.py
@@ -0,0 +1,38 @@
+import mlflow
+import pandas as pd
+import xgboost as xgb
+
+MODEL_URI = 's3://nerdward-bucket/model/'
+
+def feature_eng(data):
+    df = pd.DataFrame(data, index=[0])
+    columns = ['MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond',
+       'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2',
+       'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF',
+       'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath',
+       'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces',
+       'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF',
+       'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal',
+       'MoSold', 'YrSold']
+    X = df[columns]
+
+    return X
+
+def load_model(model_location):
+    model = mlflow.xgboost.load_model(model_location)
+
+    return model
+
+def make_prediction(model, input_data):
+    X = feature_eng(input_data)
+    X = xgb.DMatrix(X)
+    preds = model.predict(X)
+
+    return preds
+
+def lambda_handler(event, context):
+    # house = event['house']
+    model = load_model(MODEL_URI)
+    pred = make_prediction(model, event)
+
+    return {"house_price": pred.tolist()}
