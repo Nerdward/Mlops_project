2b57b696f60cce157d8df54ba59db36b3c0f76bd
Ohakim Nnaemeka <ohakim.nnaemeka@gmail.com>
2022-09-05 14:34:47 +0000

    formated and sorted python files

--- best_practices/main.py
+++ best_practices/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
+
 import predict
 
 app = FastAPI()
-MODEL_LOCATION = './model'
+MODEL_LOCATION = "./model"
+
 
 @app.on_event("startup")
 def load_artifacts():
     return predict.load_model(MODEL_LOCATION)
 
+
 @app.post("/predict")
-def _predict(request:dict):
+def _predict(request: dict):
     """Predict House price"""
     X = predict.feature_eng(request)
     model = load_artifacts()
--- best_practices/test_docker.py
+++ best_practices/test_docker.py
@@ -1,18 +1,18 @@
-import json 
-import requests
+import json
 
+import requests
 from deepdiff import DeepDiff
 
-with open("input.json", "rt",encoding="utf-8") as f:
+with open("input.json", "rt", encoding="utf-8") as f:
     house = json.load(f)
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+url = "http://localhost:8080/2015-03-31/functions/function/invocations"
 response = requests.post(url, json=house)
 print("actual response: ")
 print(response.json())
 
-expected_response = {'house_price': [109631.2265625]}
+expected_response = {"house_price": [109631.2265625]}
 diff = DeepDiff(response.json(), expected_response)
-print(f'diff={diff}')
+print(f"diff={diff}")
 
 assert "type_changes" not in diff
